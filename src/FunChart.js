1079d25ca3d1568450eca8d6768e73e868fdc81f
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-08-27 07:30:47 +0200

    added axis flow to functional Chart

--- src/FunChart.js
+++ src/FunChart.js
@@ -1,12 +1,24 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import * as d3 from 'd3'
 
 const width = 650;
 const height = 450;
+const margin = { top: 20, right: 5, bottom: 20, left: 35 };
 
 const FunChart = ({data}) => {
 
   const [bars, setBars] = useState([])
+  const [scales, setScales] = useState({})
+
+  const xAxis = useRef(null)
+  const yAxis = useRef(null)
+  
+  const renderAxis = (scaleX, scaleY) => {
+    const axisX = d3.axisBottom(scaleX)
+    const axisY = d3.axisLeft(scaleY)
+    d3.select(xAxis.current).call(axisX)
+    d3.select(yAxis.current).call(axisY)
+  }
 
   useEffect(() => {
     if (data) {
@@ -16,14 +28,14 @@ const FunChart = ({data}) => {
           d3.min(data, d => d.date),
           d3.max(data, d => d.date)
         ])
-        .range([0, width])
+        .range([margin.left, width - margin.right])
 
       const yScale = d3.scaleLinear()
         .domain([
           d3.min(data, d => d.low),
           d3.max(data, d => d.high)
         ])
-        .range([height, 0])
+        .range([height - margin.bottom, margin.top])
 
       const colorExtent = d3.extent(data, d => d.avg).reverse()
       const colorScale = d3.scaleSequential().domain(colorExtent).interpolator(d3.interpolateRdYlBu)
@@ -37,6 +49,7 @@ const FunChart = ({data}) => {
         }
       })
 
+      renderAxis(xScale, yScale)
       setBars(newBars)
     }
 
@@ -45,11 +58,15 @@ const FunChart = ({data}) => {
   
   return (
     <svg width={width} height={height}>
-      {
-        bars.map((d, i) =>
-          <rect key={i} x={d.x} y={d.y} width={2} height={d.height} fill={d.fill} />
-        )
-      }
+      <g>
+        {
+          bars.map((d, i) =>
+            <rect key={i} x={d.x} y={d.y} width={2} height={d.height} fill={d.fill} />
+          )
+        }
+      </g>
+      <g ref={xAxis} transform={`translate(0, ${height - margin.bottom})`}></g>
+      <g ref={yAxis} transform={`translate(${margin.left}, 0)`}></g>
     </svg>
   )
 }
